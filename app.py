77174743feec6c9c7d49a7ef8c2b95b067422a65
nishant-5 <76944655+nishant-5@users.noreply.github.com>
2024-09-22 16:59:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,16 @@ email_content = st.text_area("Enter A Question:", height=200)
 if st.button("Generate Answer"):
     if email_content:
         # Tokenize and generate subject line
+        model = AutoModelForCausalLM.from_pretrained("Nishantc05/qa-gptmodel")
+        device="cuda"
+        model.to(device)
+        tokenizer = GPT2Tokenizer.from_pretrained("Nishantc05/qa-gptmodel", bos_token='<|startoftext|>', eos_token='<|endoftext|>', pad_token='<|pad|>')
+        tokenizer.padding_side = "left"
+        tokenizer.pad_token=tokenizer.eos_token
         inputs = tokenizer.encode(email_content, return_tensors="pt", max_length=512, truncation=True)
         outputs = model.generate(inputs, max_length=20, num_beams=5, early_stopping=True)
         subject_line = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        st.subheader("Generated Subject Line:")
+        st.subheader("Generated Answer...")
         st.write(subject_line)
     else:
         st.write("Please enter some email content to generate a subject line.")
