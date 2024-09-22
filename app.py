949423140c0bce7ff571f229cdbccdedcdf46b67
nishant-5 <76944655+nishant-5@users.noreply.github.com>
2024-09-22 17:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,10 @@ if st.button("Generate Answer"):
         print("Email content-",email_content)
         print("Inputs-",inputs)
         outputs = model.generate(inputs, max_length=100, num_beams=5, early_stopping=True)
-        subject_line = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        subject_line = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+        unwanted_tokens = ['<ANSWER_ENDED>', email_content]
+        for token in unwanted_tokens:
+            subject_line = subject_line.replace(token, '')
         #subject_line = tokenizer.decode(skip_special_tokens=True)
         st.subheader("Generated Answer...")
         st.write(subject_line)
